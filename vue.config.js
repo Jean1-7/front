1dbf0b987c17221c2a207120221a25d3e312e909
Jean Pierre Anco Aymara <jancoay@unsa.edu.pe>
2024-07-30 16:30:25 -0500

    a

--- vue.config.js
+++ vue.config.js
@@ -1,29 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://liban.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: { '^/api': '' },
-      },
-    },
-  },
-
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
-
-  chainWebpack: config => {
-    config.module
-      .rule('images')
-      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'media/[name].[hash:8].[ext]'
-      })
-  },
+  transpileDependencies: true
 })
+
